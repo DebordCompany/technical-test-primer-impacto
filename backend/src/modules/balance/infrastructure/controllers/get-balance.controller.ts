8cb551f0a600b4097f154246106ecf55d00b8440
Debord <debord.info@gmail.com>
2026-03-16 16:03:44 +0000

    docs: add docs to balance controller

--- backend/src/modules/balance/infrastructure/controllers/get-balance.controller.ts
+++ backend/src/modules/balance/infrastructure/controllers/get-balance.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, HttpException, HttpStatus, Query } from '@nestjs/common';
 import { GetBalanceService } from '../../application/getBalance/get-balance.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiInternalServerErrorResponse, ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GetBalanceDto } from '../../application/getBalance/get-balance.dto';
 
 @Controller('balance')
@@ -10,6 +10,51 @@ export class GetBalanceController {
 
   @Get()
   @ApiOperation({ summary: 'Get balance data for a given date range' })
+  @ApiQuery({
+    name: 'start_date',
+    required: true,
+    type: String,
+    description: 'Fecha de inicio (YYYY-MM-DD)',
+    example: '2024-01-01',
+  })
+  @ApiQuery({
+    name: 'end_date',
+    required: true,
+    type: String,
+    description: 'Fecha de fin (YYYY-MM-DD)',
+    example: '2024-01-31',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Balance consultado correctamente',
+    schema: {
+      example: {
+        status: 'success',
+        data: [
+          { id: 1, datetime: '2024-01-01T01:00:00', },
+          { id: 2, datetime: '2024-01-02T01:00:00', },
+        ]
+      }
+    }
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad Request',
+    schema: {
+      example: {
+        status: "error",
+        message: "Datos de entrada inválidos"
+      }
+    }
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+    schema: {
+      example: {
+        status: "error",
+        message: "Error interno del servidor"
+      }
+    }
+  })
   async getBalance(
     @Query() query: GetBalanceDto
   ) {
